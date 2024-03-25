15ac52f339360b0000853681299d0bfe51feed51
Esmond Lam <esmondlam0923@gmail.com>
2024-03-25 11:49:07 +0800

    Implement and style Random, Grid, and Eraser button; Grid has hover effect

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const clearBtn = document.querySelector(".clear-btn");
+const gridBtn = document.querySelector(".grid-btn");
+const randomBtn = document.querySelector(".random-btn");
+const eraserBtn = document.querySelector(".eraser-btn");
 const gridContainer = document.querySelector(".grid-container");
 const gridContainerDimension = gridContainer.clientWidth;
 const sliderLabel = document.querySelector("label");
@@ -8,7 +11,10 @@ const colorPicker = document.querySelector("#color-picker");
 const grids = [];
 let numOfGridEachRow = 16;
 let isDrawing = false;
-let color = "black";
+let defaultColor = "black";
+let hasGrid = true;
+let isRandom = false;
+let isEraser = false;
 
 function createGrid() {
   const fragment = document.createDocumentFragment();
@@ -16,10 +22,13 @@ function createGrid() {
 
   sliderLabel.textContent = `${numOfGridEachRow} * ${numOfGridEachRow} grids`;
   for (let i = 0; i < totalNumofGrids; i++) {
-    const grid = document.createElement("p");
+    const grid = document.createElement("div");
     const gridDimension = gridContainerDimension / numOfGridEachRow;
-    grid.style.cssText = `width: ${gridDimension}px; height: ${gridDimension}px`;
-    grid.style.border = "1px lightgrey solid";
+
+    grid.style.cssText = `width: ${gridDimension}px; height: ${gridDimension}px; cursor: pointer;`;
+
+    if (hasGrid) grid.style.border = "1px solid lightgrey";
+
     fragment.appendChild(grid);
     grids.push(grid);
   }
@@ -27,13 +36,22 @@ function createGrid() {
 }
 
 function setBackgroundColor(event) {
+  let color;
+
   if (isDrawing) {
     const curBrightness = event.target.style.filter || "100";
     const curBrightnessValue = curBrightness.replace(/\D/g, "");
 
-    if (event.target.classList.contains("painted")) {
+    if (event.target.classList.contains("painted") && !isEraser) {
       event.target.style.filter = `brightness(${curBrightnessValue - 10}%)`;
+    } else {
+      event.target.style.filter = `brightness(100%)`;
     }
+
+    if (isRandom)
+      color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    else if (isEraser) color = "";
+    else color = defaultColor;
 
     event.target.classList.add("painted");
     event.target.style.backgroundColor = color;
@@ -46,27 +64,69 @@ function removeGrid() {
   }
 }
 
-function clearGrid() {
-  for (const grid of grids) {
-    grid.style.backgroundColor = "white";
-    grid.style.filter = "brightness(100%)";
-    grid.classList.remove("painted");
+function toggleGridLine() {
+  for (let grid of grids) {
+    if (hasGrid) {
+      grid.style.border = "1px solid lightgrey";
+    } else {
+      grid.style.border = "none";
+    }
   }
 }
 
-colorPicker.addEventListener("change", (event) => {
-  color = event.target.value;
-});
-
 function handleGridEvent() {
-  gridContainer.addEventListener("click", () => (isDrawing = !isDrawing));
+  gridContainer.addEventListener("click", (event) => {
+    isDrawing = !isDrawing;
+  });
   gridContainer.addEventListener("mouseover", setBackgroundColor);
   gridContainer.addEventListener("mouseleave", () => {
-    if (isDrawing) isDrawing = false;
+    isDrawing = false;
   });
 }
 
-clearBtn.addEventListener("click", clearGrid);
+function disableRandom() {
+  isRandom = false;
+  if (randomBtn.classList.contains("selected")) {
+    randomBtn.classList.remove("selected");
+  }
+}
+
+function disableEraser() {
+  isEraser = false;
+  if (eraserBtn.classList.contains("selected")) {
+    eraserBtn.classList.remove("selected");
+  }
+}
+
+gridBtn.addEventListener("click", () => {
+  hasGrid = !hasGrid;
+  gridBtn.classList.toggle("selected");
+  toggleGridLine();
+});
+
+clearBtn.addEventListener("click", () => {
+  removeGrid();
+  createGrid();
+});
+
+randomBtn.addEventListener("click", () => {
+  isRandom = !isRandom;
+  disableEraser();
+  randomBtn.classList.toggle("selected");
+});
+
+eraserBtn.addEventListener("click", () => {
+  isEraser = !isEraser;
+  disableRandom();
+  eraserBtn.classList.toggle("selected");
+});
+
+colorPicker.addEventListener("change", (event) => {
+  defaultColor = event.target.value;
+  disableEraser();
+  disableRandom();
+});
+
 sliderInput.addEventListener("input", (event) => {
   numOfGridEachRow = event.target.value;
   removeGrid();
